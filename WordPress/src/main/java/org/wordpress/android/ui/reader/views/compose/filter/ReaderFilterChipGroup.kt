12f1dc62715f45e54fca3d0fd3e58399269fdb88
Nick Bradbury <nick.bradbury@gmail.com>
2024-10-31 13:54:03 -0400

    Fixed ReaderFilterChipGroupPreview

--- WordPress/src/main/java/org/wordpress/android/ui/reader/views/compose/filter/ReaderFilterChipGroup.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/reader/views/compose/filter/ReaderFilterChipGroup.kt
@@ -16,19 +16,18 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -220,26 +219,16 @@ data class ReaderFilterSelectedItem(
 @Preview(name = "Dark Mode", showBackground = true, uiMode = UI_MODE_NIGHT_YES)
 @Composable
 fun ReaderFilterChipGroupPreview() {
-    var selectedItem: ReaderFilterSelectedItem? by rememberSaveable { mutableStateOf(null) }
-
+    val selectedItem: ReaderFilterSelectedItem? by rememberSaveable { mutableStateOf(null) }
     AppThemeM3WithoutBackground {
         ReaderFilterChipGroup(
             modifier = Modifier.padding(Margin.Medium.value),
             selectedItem = selectedItem,
             blogsFilterCount = 23,
             tagsFilterCount = 41,
-            onFilterClick = { type ->
-                selectedItem = ReaderFilterSelectedItem(
-                    text = UiString.UiStringText("Amazing ${type.name.lowercase()}"),
-                    type = type,
-                )
-            },
-            onSelectedItemClick = {
-                selectedItem = null
-            },
-            onSelectedItemDismissClick = {
-                selectedItem = null
-            },
+            onFilterClick = { },
+            onSelectedItemClick = { },
+            onSelectedItemDismissClick = { },
         )
     }
 }
