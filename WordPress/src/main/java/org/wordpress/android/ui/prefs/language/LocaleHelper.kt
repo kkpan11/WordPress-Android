a4600ea4a8102ee95dddf814f73fede136850138
Nick Bradbury <nick.bradbury@gmail.com>
2024-12-04 15:42:19 -0500

    Added ability to reset app locale

--- WordPress/src/main/java/org/wordpress/android/ui/prefs/language/LocaleHelper.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/prefs/language/LocaleHelper.kt
@@ -8,7 +8,7 @@ import javax.inject.Inject
 class LocaleHelper @Inject constructor() {
     private fun getCurrentLocale(): Locale {
         val locales = getApplicationLocaleList()
-        return if (locales.isEmpty) {
+        return if (locales.isEmpty || locales == LocaleListCompat.getEmptyLocaleList()) {
             Locale.getDefault()
         } else {
             locales[0] ?: Locale.getDefault()
@@ -22,6 +22,12 @@ class LocaleHelper @Inject constructor() {
      * https://developer.android.com/reference/androidx/appcompat/app/AppCompatDelegate#getApplicationLocales()
      */
     private fun getApplicationLocaleList() = AppCompatDelegate.getApplicationLocales()
+
+    // Useful during testing to clear the stored app locale
+    @Suppress("unused")
+    private fun resetApplicationLocale() {
+        AppCompatDelegate.setApplicationLocales(LocaleListCompat.getEmptyLocaleList())
+    }
 
     fun setCurrentLocaleByLanguageCode(languageCode: String) {
         // We shouldn't have to replace "_" with "-" but this is in order to work with our language picker
