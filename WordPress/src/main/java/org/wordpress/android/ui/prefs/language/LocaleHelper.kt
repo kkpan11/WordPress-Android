33b0ad81da6c28a6af6734f9eb5a081b3fd37873
Nick Bradbury <nick.bradbury@gmail.com>
2024-12-04 11:44:31 -0500

    Improved LocaleHelper

--- WordPress/src/main/java/org/wordpress/android/ui/prefs/language/LocaleHelper.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/prefs/language/LocaleHelper.kt
@@ -5,9 +5,16 @@ import androidx.core.os.LocaleListCompat
 import java.util.Locale
 
 class LocaleHelper {
-    private fun getCurrentLocale(): Locale? = AppCompatDelegate.getApplicationLocales()[0]
+    private fun getCurrentLocale(): Locale {
+        val locales = AppCompatDelegate.getApplicationLocales()
+        return if (locales.isEmpty) {
+            Locale.getDefault()
+        } else {
+            locales[0] ?: Locale.getDefault()
+        }
+    }
 
-    fun getCurrentLocaleDisplayName(): String = getCurrentLocale()?.displayName ?: ""
+    fun getCurrentLocaleDisplayName(): String = getCurrentLocale().displayName
 
     fun setCurrentLocaleByLanguageCode(languageCode: String) {
         val appLocale = LocaleListCompat.forLanguageTags(languageCode)
