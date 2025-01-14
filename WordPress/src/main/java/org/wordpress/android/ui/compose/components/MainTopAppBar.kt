4c0a55580a628e3010c7a489c7114bab56eea8ac
Nick Bradbury <nick.bradbury@gmail.com>
2025-01-14 15:22:52 -0500

    Fixed MainTopAppBar colors

--- WordPress/src/main/java/org/wordpress/android/ui/compose/components/MainTopAppBar.kt
+++ WordPress/src/main/java/org/wordpress/android/ui/compose/components/MainTopAppBar.kt
@@ -12,6 +12,7 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -92,8 +93,10 @@ fun MainTopAppBar(
                 elevation = elevation,
             )
         ),
-        // backgroundColor = backgroundColor,
-        // contentColor = contentColor,
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = backgroundColor,
+            titleContentColor = contentColor,
+        ),
         title = { Text(text = title ?: "") },
         navigationIcon = {
             navigationIcon?.let {
